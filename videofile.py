e40ccdaa79ce040bd44ef1d880796a9bd50e310c
Seiya-Umemoto <seiya.u77@gmail.com>
2020-05-25 23:51:11 +0900

    calculating values of error

--- videofile.py
+++ videofile.py
@@ -3,10 +3,10 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 # Same command function as streaming, its just now we pass in the file path, nice!
-cap = cv2.VideoCapture('/Users/2018A00587/Desktop/embedded_system/sample3_rs.mp4')
+cap = cv2.VideoCapture('/Users/2018A00587/Desktop/embedded_system/sample3_rs2.mp4')
 
 # FRAMES PER SECOND FOR VIDEO
-fps = 25
+fps = 15
 
 pts1 = np.array([[62,135],[340,135],[45,185],[362,185]],np.int32)
 pts2 = np.array([[100,0],[280,0],[100,240],[280,240]],np.int32)
@@ -15,9 +15,6 @@ def Perspective(image, pts1): #원근법 변환
     """
     Capture a Region of Interest
     """
-    # for i, pt in enumerate(pts1):
-    #     cv2.line(image,pt1=tuple(pt), pt2=tuple(pts1[i-3]),color=(255,0,0),thickness=2)
-    # cv2.polylines(image,[pts1],isClosed=True,color=(255,0,0),thickness=2)
     # cv2.line(image,pt1=tuple(pts1[0]),pt2=tuple(pts1[1]),color=(255,0,0),thickness=2)
     # cv2.line(image,pt1=tuple(pts1[1]),pt2=tuple(pts1[3]),color=(255,0,0),thickness=2)
     # cv2.line(image,pt1=tuple(pts1[3]),pt2=tuple(pts1[2]),color=(255,0,0),thickness=2)
@@ -27,52 +24,74 @@ def Perspective(image, pts1): #원근법 변환
     # cv2.line(image,pt1=tuple(pts2[1]),pt2=tuple(pts2[3]),color=(255,0,0),thickness=2)
     # cv2.line(image,pt1=tuple(pts2[3]),pt2=tuple(pts2[2]),color=(255,0,0),thickness=2)
     # cv2.line(image,pt1=tuple(pts2[2]),pt2=tuple(pts2[0]),color=(255,0,0),thickness=2)
+
     Matrix = cv2.getPerspectiveTransform(pts1.astype(np.float32), pts2.astype(np.float32))
     imgPers = cv2.warpPerspective(image, Matrix, (400, 240))
     return imgPers
 
 def Threshold(imgPers):
     imgGray = cv2.cvtColor(imgPers, cv2.COLOR_RGB2GRAY)
-    imgThresh = cv2.inRange(imgGray, 150, 255, cv2.THRESH_BINARY)
-    cv2.imshow('binary threshold', imgThresh)
-    cv2.waitKey(0)
-    imgEdge = cv2.Canny(imgGray, 50, 200)
-    imgFinal = cv2.add(imgThresh, imgEdge)
+
+    # Blurring -----------------------------------------------
+    # imgBlur = cv2.blur(imgPers, (5,5))
+    imgBlur = cv2.GaussianBlur(imgGray, (5,5), 0)
+    # imgBlur = cv2.medianBlur(imgPers, 5)
+    # imgBlur = cv2.bilateralFilter(imgPers,9,75,75)
+    # -----------------------------------------------
+
+    imgThresh = cv2.inRange(imgBlur, 149, 255, cv2.THRESH_BINARY)
+
+    # Erosion / Dilation-------------------------------------------------------
+    kernel = np.ones((5,5),np.uint8)
+    imgErode = cv2.erode(imgThresh,kernel,iterations=1) 
+    # imgDilate = cv2.dilate(imgErode,kernel,iterations=1) 
+    # -------------------------------------------------------
+
+    sobel_horizontal = cv2.Sobel(imgErode, cv2.CV_8U, 1, 0, ksize=9)
+    # kernel2 = np.ones((3,3),np.uint8)
+    # imgMpl = cv2.morphologyEx(sobel_horizontal, cv2.MORPH_CLOSE, kernel2)
+    
+    # Canny edge detection : still need tuning
+    imgEdge = cv2.Canny(imgGray, 270, 300)
+
+
+    imgFinal = cv2.add(sobel_horizontal, imgEdge)
     imgFinal = cv2.cvtColor(imgFinal, cv2.COLOR_GRAY2RGB)
     imgFinalDuplicate = cv2.cvtColor(imgFinal, cv2.COLOR_RGB2BGR)
     imgFinalDuplicate1 = cv2.cvtColor(imgFinal, cv2.COLOR_RGB2BGR)
     return imgFinal, imgFinalDuplicate, imgFinalDuplicate1
 
-def Histrogram(imgFinalDuplicate):
-    histrogramLane = np.float32([])
+def Histogram(imgFinalDuplicate):
+    histogramLane = np.uint8([])
     
     for i in range(400):
-        # cv2.imshow('img',imgFinalDuplicate)
-        ROILane = cv2.rectangle(imgFinalDuplicate,(i,140),(i+1,240),(0,0,255),3)
+        # ROILane = cv2.rectangle(imgFinalDuplicate,(i,140),(i+1,240),(0,0,255),3)
+        # Matrix: rows 100, cols 1 
+        ROILane= imgFinalDuplicate[140:240, i]
+        # scaling (0 ~ 255) to (0 ~ 1)
         ROILane = cv2.divide(ROILane, 255)
-        print(ROILane.shape)
-        print(ROILane.sum(axis=2))
-        np.append(histrogramLane, int(sum(ROILane)[0]))
-    return histrogramLane
+        # summation of white pixels: append the number of white pixels in ROILane to hisogramLane  
+        histogramLane = np.append(histogramLane, ROILane.sum(axis=0)[0:1].astype(np.uint8), axis=0)
+    return histogramLane
 
 
 def LaneFinder(imgFinal, histrogramLane):
-    LeftPtr = np.argmax(histrogramLane[:150])
-    LeftLanePos = np.diff(histrogramLane[0], LeftPtr)
+    # find the position of left edge of left lane
+    LeftLanePos = np.argmax(histrogramLane[:150])
 
-    RightPtr = np.argmax(histrogramLane[250:])
-    RightLanePos = np.diff(histrogramLane[0], LeftPtr)
+    # find the position of left edge of right lane
+    RightLanePos = 250 + np.argmax(histrogramLane[250:])
 
-    cv2.line(imgFinal, [LeftLanePos, 0], [LeftLanePos, 240], color=(0,255,0), thickness=2)
-    cv2.line(imgFinal, [RightLanePos, 0], [RightLanePos, 240], color=(0,255,0), thickness=2)
+    cv2.line(imgFinal, (LeftLanePos, 0), (LeftLanePos, 240), color=(0,255,0), thickness=2)
+    cv2.line(imgFinal, (RightLanePos, 0), (RightLanePos, 240), color=(0,255,0), thickness=2)
     return LeftLanePos, RightLanePos
 
 def LaneCenter(imgFinal, LeftLanePos, RightLanePos):
-    laneCenter = (RightLanePos - LeftLanePos) / 2 + LeftLanePos
+    laneCenter = ((RightLanePos - LeftLanePos) / 2 + LeftLanePos).astype(np.uint8)
     frameCenter = 188
 
-    cv2.line(imgFinal, [laneCenter, 0], [laneCenter, 240], color=(0,255,0), thickness=3)
-    cv2.line(imgFinal, [frameCenter, 0], [frameCenter, 240], color=(255,0,0), thickness=3)
+    cv2.line(imgFinal, (laneCenter, 0), (laneCenter, 240), color=(0,255,0), thickness=3)
+    cv2.line(imgFinal, (frameCenter, 0), (frameCenter, 240), color=(255,0,0), thickness=3)
 
     Result = laneCenter - frameCenter
     return Result
@@ -92,12 +111,12 @@ while cap.isOpened():
     if ret == True:
         imgPers = Perspective(frame, pts1)
         imgFinal, imgFinalDuplicate, imgFinalDuplicate1 = Threshold(imgPers)
-        histrogramLane = Histrogram(imgFinalDuplicate)
-        LeftLanePos, RightLanePos = LaneFinder(imgFinal, histrogramLane)
+        histogramLane = Histogram(imgFinalDuplicate)
+        LeftLanePos, RightLanePos = LaneFinder(imgFinal, histogramLane)
         Result = LaneCenter(imgFinal, LeftLanePos, RightLanePos)
     
-        str(ss(f"Result = {Result}")) #StringStream
-        cv2.putText(img, str(ss), [1,50], 0, 1, color=(0,0,255), thickness=2)        
+        ss = f"Result = {Result}" #StringStream
+        cv2.putText(frame, str(ss), (1,50), 0, 1, color=(0,0,255), thickness=2)        
 
          # Display the frame at same frame rate of recording
         # Watch lecture video for full explanation
